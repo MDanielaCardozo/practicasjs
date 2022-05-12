75bbecb2686b1c14bb5213e639a9ced4f1f89194
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-12 11:11:13 -0300

    agregamos conceptos de variables

---
+++ js/1-variables.js
@@ -0,0 +1,45 @@
+//Esto es un comentario simple
+
+/*Esto es un 
+comentario de
+varias lineas*/
+
+// mostrar un comentario en la pantalla
+document.write("Hola mundo");
+
+// mostrar un mensaje en la consola
+console.log('Hola mundo');
+
+// mostrar un mensaje en una ventana emergente
+// alert('Hola comision 2i');
+
+// crear,declarar, inicializar variables (var - let -const)
+
+let persona = 'Nico Andrade';
+var persona2 = 'Daniela';
+
+document.write('<br>Persona: '+ persona);
+document.write('<br>Segunda persona: '+ persona2);
+
+// cambiar el valor de uan variable, no se repite let
+
+persona = 'Francisco';
+document.write('<br>Persona: '+ persona);
+
+// constantes
+
+const url = 'http://127.0.0.1:5500/index.html';
+document.write('<br>URL: '+ url);
+
+let anio = 1997;
+document.write('<br>Año de Nac: '+ anio);
+
+let foco = false;
+foco = true;
+
+document.write('<br>Foco '+ foco)
+
+
+
+
+
