4809364f0b27e573803e2c0e7a3f057624519e85
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-16 16:52:19 -0300

    Primer commit

---
+++ js/ejercicios/ejercicio-5-tp1.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt('Escriba un numero entero'));
+let numero2 = parseInt(prompt('Escriba un segundo numero entero'));
+// console.log(numero1)
+// console.log(numero2)
+document.write('Resultado: '+ (numero1 + numero2));
+
+// parse_Int('500');
