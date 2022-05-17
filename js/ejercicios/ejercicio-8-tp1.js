01b5df1afdac3d909c1aa516b4c97cb0c8a05f71
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-17 19:41:09 -0300

    Ejercicio ocho check

---
+++ js/ejercicios/ejercicio-8-tp1.js
@@ -0,0 +1,9 @@
+// 8.- Escribe un programa que pida un número y diga si es divisible por 2
+
+let numero = parseInt(prompt('Ingrese un numero'))
+
+if (numero % 2 == 0) {
+    document.write('El numero ingresado es divisible por dos')
+} else {
+    document.write('El numero ingresado no es divisible por dos')
+}
