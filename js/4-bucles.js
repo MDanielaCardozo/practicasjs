56d758cfa9ab313fa7e623bdcb78a6b9e99ab965
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-19 11:24:05 -0300

    agregué otra solucion con for para el ejercicio 9

---
+++ js/4-bucles.js
@@ -0,0 +1,61 @@
+// document.write('Elemento 1<br>');
+// document.write('Elemento 2<br>');
+// document.write('Elemento 3<br>');
+// document.write('Elemento 4<br>');
+// document.write('Elemento 5<br>');
+
+/* while
+while(condicion logica){
+    todo  el codigo que quiero repetir varias veces
+    cambiar la condicion logica
+}
+*/
+
+ let contador = 8;
+
+// while (contador <= 5){
+//     // document.write('Elemento '+contador+'<br>');
+//     document.write(`Elemento ${contador} <br>`);
+//     contador++;  // contador = contador +1; 
+// }
+
+/*
+do-while{
+    todo lo que quiero que se repita
+
+}while(condicion logica)
+*/
+
+// do{
+//     document.write(`Elemento ${contador} <br>`);
+//     contador++;
+// }while(contador<=5)
+
+// Estructura For
+
+/* for (inicializar una variable; condicion logica; incrementar o decrementar la variable inicializada){
+    todo el codigo que quiero que se repita
+} 
+
+*/
+
+for(let indice = 5; indice >= 1; indice--){
+    document.write(`Elemento ${indice} <br>`);
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/ejercicios/ejercicio-9-tp1.js
+++ js/ejercicios/ejercicio-9-tp1.js
@@ -6,24 +6,30 @@ frase = frase.toLowerCase();
 console.log(frase.length);
 console.log(frase.substring(0,5));
 
-if(frase.substring(0,1) === 'a' || frase.substring(0,1) === 'e' || frase.substring(0,1) === 'i' || frase.substring(0,1) === 'o' || frase.substring(0,1) === 'u'){
-    document.write(frase.substring(0,1))
-}
+for(let indice = 0; indice < 5; indice++){
+    if(frase.charAt(indice) === 'a' || frase.charAt(indice) === 'e' || frase.charAt(indice) === 'o'
+|| frase.charAt(indice) === 'u'){
+    document.write(frase.charAt(indice));
+}}
 
-if(frase.substring(1,2) === 'a' || frase.substring(1,2) === 'e' || frase.substring(1,2) === 'i' || frase.substring(1,2) === 'o' || frase.substring(1,2) === 'u'){
-    document.write(frase.substring(1,2))
-}
+// if(frase.substring(0,1) === 'a' || frase.substring(0,1) === 'e' || frase.substring(0,1) === 'i' || frase.substring(0,1) === 'o' || frase.substring(0,1) === 'u'){
+//     document.write(frase.substring(0,1))
+// }
 
-if(frase.substring(2,3) === 'a' || frase.substring(2,3) === 'e' || frase.substring(2,3) === 'i' || frase.substring(2,3) === 'o' || frase.substring(2,3) === 'u'){
-    document.write(frase.substring(2,3))
-}
+// if(frase.substring(1,2) === 'a' || frase.substring(1,2) === 'e' || frase.substring(1,2) === 'i' || frase.substring(1,2) === 'o' || frase.substring(1,2) === 'u'){
+//     document.write(frase.substring(1,2))
+// }
 
-if(frase.substring(3,4) === 'a' || frase.substring(3,4) === 'e' || frase.substring(3,4) === 'i' || frase.substring(3,4) === 'o' || frase.substring(3,4) === 'u'){
-    document.write(frase.substring(3,4))
-}
+// if(frase.substring(2,3) === 'a' || frase.substring(2,3) === 'e' || frase.substring(2,3) === 'i' || frase.substring(2,3) === 'o' || frase.substring(2,3) === 'u'){
+//     document.write(frase.substring(2,3))
+// }
 
-if(frase.substring(4,5) === 'a' || frase.substring(4,5) === 'e' || frase.substring(4,5) === 'i' || frase.substring(4,5) === 'o' || frase.substring(4,5) === 'u'){
-    document.write(frase.substring(4,5))
-}
+// if(frase.substring(3,4) === 'a' || frase.substring(3,4) === 'e' || frase.substring(3,4) === 'i' || frase.substring(3,4) === 'o' || frase.substring(3,4) === 'u'){
+//     document.write(frase.substring(3,4))
+// }
+
+// if(frase.substring(4,5) === 'a' || frase.substring(4,5) === 'e' || frase.substring(4,5) === 'i' || frase.substring(4,5) === 'o' || frase.substring(4,5) === 'u'){
+//     document.write(frase.substring(4,5))
+// }
 
 // if(frase.charAt(0) === 'a' || frase.charAt(0) === 'e')
